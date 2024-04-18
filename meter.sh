1c242aa8a45a709fe577641749c268834cd5881e
javier <alu0101562445@ull.edu.es>
2024-04-18 14:34:59 +0100

    Thu Apr 18 14:34:59 WEST 2024

--- meter.sh
+++ meter.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # VARIABLES
-DIRECTORIO=./5ºpractica/src/
+DIRECTORIO=./6ºpractica/src/
 PROGRAMA=
-numero_de_practica="05"
-nombre_de_practica="Algoritmos de ordenación"
+numero_de_practica="06"
+nombre_de_practica="Árbol (TAD)"
 # PROGRAMAS
 mkdir "$DIRECTORIO"
 while [ "$1" != "" ]; do
   PROGRAMA=$1
   shift
-  mkdir "$DIRECTORIO""$PROGRAMA"
-  touch "$DIRECTORIO"/"$PROGRAMA"/"$PROGRAMA".cc
+  mkdir "$DIRECTORIO""$PROGRAMA" # creo los directorios individuales para cada clase
+  touch "$DIRECTORIO"/"$PROGRAMA"/"$PROGRAMA".cc # creo los archivos .cc
   cat formatocc.txt | sed \
     -e "s/¬/$PROGRAMA.cc/g" \
     -e "s|\*date*|$(date +%D)|g" \
@@ -19,15 +19,15 @@ while [ "$1" != "" ]; do
     -e "s|\*numero|$numero_de_practica|g" \
     -e "s|\*nombre_practica|$nombre_de_practica|g" > "$DIRECTORIO"/"$PROGRAMA"/"$PROGRAMA".cc 
   
-  touch "$DIRECTORIO"/"$PROGRAMA"/"$PROGRAMA".h
+  touch "$DIRECTORIO"/"$PROGRAMA"/"$PROGRAMA".h # creo los archivos .h
   cat formatoh.txt | sed -e "s/¬/$PROGRAMA.h/g" -e "s|\*date*|$(date +%D)|g" -e "s|\*programa_sin_cc|$PROGRAMA|g" -e "s|\*numero|$numero_de_practica|g" -e "s|\*nombre_practica|$nombre_de_practica|g" > "$DIRECTORIO"/"$PROGRAMA"/"$PROGRAMA".h
 done
-touch "$DIRECTORIO"main.cc
+touch "$DIRECTORIO"main.cc # creo el archivo main.cc
 cat formatocc.txt | sed \
     -e "s/¬/main.cc/g" \
     -e "s|\*date*|$(date +%D)|g" \
     -e "s|\*programa_sin_cc|main|g" \
     -e "s|\*numero|$numero_de_practica|g" \
     -e "s|\*nombre_practica|$nombre_de_practica|g" > "$DIRECTORIO"main.cc 
-touch "$DIRECTORIO"../makefile
+touch "$DIRECTORIO"../makefile # creo el archivo makefile
 cat makefile > "$DIRECTORIO"../makefile
